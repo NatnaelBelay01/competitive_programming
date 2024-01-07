ad1d15d002e5b0736c07a0e8b025a5cdd5bbbc41
NatnaelBelay01 <101341937+NatnaelBelay01@users.noreply.github.com>
2024-01-07 19:06:04 +0300

    Added solution for 3Sum

---
+++ week1/3sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        result = {}
+        for idx in range(len(nums)):
+
+            store = {}
+            check = -1 * nums[idx]
+            for idx2 in range(len(nums)):
+
+                if(idx2 == idx):
+                    continue
+
+                if (nums[idx2] in store):
+                    temp = []
+                    temp.append(-1*check)
+                    temp.append(store[nums[idx2]])
+                    temp.append(nums[idx2])
+                    temp.sort()
+                    result[tuple(temp)] = 1 + result.get(tuple(temp) , 0)
+                store[check - nums[idx2]] = nums[idx2]
+        return [i for i in result]
